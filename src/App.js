eed2fb85716033d348d61570f34e8b87520d8140
Jay <vijayasimhabr@live.com>
2023-02-09 12:36:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import SimpleLandingPage from "./components/SimpleLandingPage";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <p>making this video tutorial to show how to use code sandbox </p>
+      </div>
+      <SimpleLandingPage />
+    </div>
+  );
+}
---
+++ src/components/SimpleLandingPage.js
@@ -0,0 +1,22 @@
+//SimpleLandingPage.js
+
+const SimpleLandingPage = () => (
+  <div>
+    <div className="container-fluid text-center">
+      <h1>Hello There.Using Code Sandbox from Simple Landing Page Component</h1>
+      <hr></hr>
+      <img
+        src="https://sandkdesignstudioin.files.wordpress.com/2022/10/showcaseoneoctober13thwatermarksete.jpg"
+        className="img-fluid"
+        alt="..."
+      ></img>
+      <hr></hr>
+      <h5>
+        image from my own photography hobby
+        <a href="https://sandkdesignstudio.in/portfolio/"> source </a>
+      </h5>
+    </div>
+  </div>
+);
+
+export default SimpleLandingPage;
